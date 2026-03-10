4ef39e16be34b89ef3b81f94a34757f7e13b7115
Adrián Calvo <adrian11062007@gmail.com>
2026-03-10 11:06:31 +0100

    Métodos básicos de la interfaz de texto.

---
+++ src/jcolonia/daw2025/tablasmvc/VistaGeneral.java
@@ -0,0 +1,16 @@
+package jcolonia.daw2025.tablasmvc;
+
+public class VistaGeneral {
+
+	public VistaGeneral() {
+		
+	}
+	
+	public void pedirNúmero() {
+		
+	}
+	
+	public void mostrarAviso() {
+		
+	}
+}
