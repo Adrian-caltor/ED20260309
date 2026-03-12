5d1f6224fca84af15f6bd3e5b18f7ef3de243076
Adrián Calvo <adrian11062007@gmail.com>
2026-03-12 10:18:17 +0100

    Clase de excepciones del programa.

---
+++ src/jcolonia/daw2025/tablasmvc/ExcepcionES.java
@@ -0,0 +1,18 @@
+package jcolonia.daw2025.tablasmvc;
+
+/**
+ * Clase de Excepciones personalizadas para el programa
+ */
+
+public class ExcepcionES extends Exception{
+
+
+	/**
+	 * Constructor excepciones
+	 * @param mensaje del error
+	 */
+	
+	public ExcepcionES(String mensaje) {
+		super(mensaje);
+	}
+}
